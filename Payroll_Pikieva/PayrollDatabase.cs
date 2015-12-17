6dab8dd775141a70ad34bd0151038bc4c58baaf9
Surishka <Surishka79@outlook.com>
2015-12-17 18:25:22 +0300

    Sinhron

---
+++ Payroll_Pikieva/PayrollDatabase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Payroll_Pikieva
+{
+    public class PayrollDatabase
+    {
+        private static Hashtable employees = new Hashtable();
+       
+        public static void AddEmployee(int id, Employee employee)
+        {
+            employees[id] = employee;
+        }
+        public static Employee GetEmployee(int id)
+        {
+            return employees[id] as Employee;
+        }
+    }
+}
---
+++ Tests_Pikieva/Tests_Pikieva.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Payroll_Pikieva;
+using System.Text;
+
+namespace Tests_Pikieva
+{
+    [TestClass]
+    public class Tests_Pikieva
+    {
+        [TestMethod]
+        public void TestEmployee()
+        {
+            int empId = 1;
+            Employee e = new Employee(empId, "Bob", "Home");
+            Assert.AreEqual("Bob", e.Name);
+            Assert.AreEqual("Home", e.Address);
+            Assert.AreEqual(empId, e.EmpId);
+        }
+        [TestMethod]
+        public void TestEmployeeToString()
+        {
+            int empId = 1;
+            string address = "Makha4kala";
+            string name = "Suriya";
+            string classification = "SYSadmin";
+            string schedule = "flexible";
+            string method = "cash";
+            StringBuilder e = new StringBuilder();
+            e.Append("Emp#: ").Append(empId).Append("   ");
+            e.Append(name).Append("   ");
+            e.Append(address).Append("   ");
+            e.Append("Paid ").Append(classification).Append("  ");
+            e.Append(schedule);
+            e.Append(" by ").Append(method);
+        }
+        [TestMethod]
+        public void TestAddSalariedEmployee()
+        {
+            int empId = 1;
+            AddSalariedEmployee t = new AddSalariedEmployee(empId, "Bob", "Home", 1000.00);
+            t.Execute();
+            Employee e = PayrollDatabase.GetEmployee(empId);
+            Assert.AreEqual("Bob", e.Name);
+            PaymentClassification pc = e.Classification;
+            Assert.IsTrue(pc is SalariedClassification);
+            SalariedClassification sc = pc as SalariedClassification;
+            Assert.AreEqual(1000.00, sc.Salary, .001);
+            PaymentSchedule ps = e.Schedule;
+            Assert.IsTrue(ps is MonthlySchedule);
+            PaymentMethod pm = e.Method;
+            Assert.IsTrue(pm is HoldMethod);
+
+
+        }
+    }
+}
